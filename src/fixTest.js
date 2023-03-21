417c867e5ddb4f2131a48262ea9b64975a53f7a1
Andrei Istrate <istrate.andrei1@yahoo.com>
2023-03-21 21:17:56 +0200

    add existing three.js project

---
+++ src/fixTest.js
@@ -0,0 +1,256 @@
+
+// Fix Top Bars Middle Model doorType=double
+switch (doorModel_left + doorType) {
+    case "cleardouble":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "polo3double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "polo3+double":
+        top1divMiddle.display = "flex";
+        top2divMiddle.display = "flex";
+        top3divMiddle.borderLeftWidth = "7px";
+        break;
+    case "polo3↓double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "polo4double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "polo4+double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "polo4↓double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "flexidouble":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "flexi+double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "flexi+↑double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "flexi+↓double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "polo5double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "polo5+double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "polo5↓double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "elo1←double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        top2divMiddle.width = "250%";
+        break;
+    case "elo1→double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        top1divMiddle.width = "250%";
+    case "elo1-double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "elo2←double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        top2divMiddle.width = "250%";
+        break;
+    case "elo2→double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        top1divMiddle.width = "250%";
+        break;
+    case "elo2-double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "centrodouble":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "centro+double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "centro-double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "diamentdouble":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "slimdouble":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        break;
+    case "cross1→double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        top1divMiddle.width = "180%";
+        break;
+    case "cross1←double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        top2divMiddle.width = "180%";
+        break;
+    case "cross2→double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        top1divMiddle.width = "180%";
+        break;
+    case "cross2←double":
+        top1divMiddle.visibility = "hidden";
+        top2divMiddle.visibility = "hidden";
+        top2divMiddle.width = "180%";
+        break;
+}
+
+
+// Fix Top Bars Middle Model doorType=double
+switch (doorModel_right + doorType) {
+    case "cleardouble":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "polo3double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "polo3+double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "polo3↓double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "polo4double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "polo4+double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "polo4↓double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "flexidouble":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "flexi+double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "flexi+↑double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "flexi+↓double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "polo5double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "polo5+double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "polo5↓double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "elo3←double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        top4divMiddle.width = "450%";
+        break;
+    case "elo3→double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        top3divMiddle.width = "450%";
+    case "elo3-double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "elo4←double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        top4divMiddle.width = "450%";
+        break;
+    case "elo4→double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        top3divMiddle.width = "450%";
+        break;
+    case "elo4-double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "centrodouble":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "centro+double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "centro-double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "diamentdouble":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "slimdouble":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        break;
+    case "cross3→double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        top3divMiddle.width = "380%";
+        break;
+    case "cross3←double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        top4divMiddle.width = "380%";
+        break;
+    case "cross4→double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        top3divMiddle.width = "380%";
+        break;
+    case "cross4←double":
+        top3divMiddle.visibility = "hidden";
+        top4divMiddle.visibility = "hidden";
+        top4divMiddle.width = "380%";
+        break;
+} 
